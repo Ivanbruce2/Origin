349a0eb9b38b57bef0bd7a8fafb37cfbf47eb840
Ivan <Ivanbruce@hotmail.com>
2023-10-10 07:50:57 -0700

    commit

--- server.js
+++ server.js
@@ -1,29 +1,49 @@
 // server.js
 const express = require('express');
 const multer = require('multer');
-const path = require('path');
+const AWS = require('aws-sdk');
 const app = express();
-const PORT = 3000; // use process.env.PORT for production
+const PORT = 3000;
 
-// Setup Multer and define where to save the uploaded files
-const storage = multer.diskStorage({
-    destination: 'uploads/',
-    filename: function(req, file, cb){
-        cb(null, Date.now() + path.extname(file.originalname));
-    }
+// Use dotenv to load environment variables
+require('dotenv').config();
+
+// Configure AWS SDK
+AWS.config.update({
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+    region: process.env.AWS_REGION  // Updated line
 });
+
+const s3 = new AWS.S3();
+
+// Multer storage configuration
+const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
 
-// Serve HTML form at root
 app.use(express.static('public'));
-
-// Serve uploaded files
-app.use('/uploads', express.static('uploads'));
 
 // Handle file uploads
 app.post('/upload', upload.single('banner'), (req, res) => {
     if(req.file) {
-        res.send(`File uploaded! <a href="/uploads/${req.file.filename}">View file</a>`);
+        // Parameters for S3 upload
+        const params = {
+            Bucket: process.env.AWS_BUCKET_NAME,  // Updated line
+            Key: req.file.originalname,
+            Body: req.file.buffer,
+            ContentType: req.file.mimetype
+        };
+
+        // Upload to S3
+        s3.upload(params, function(err, data) {
+            if (err) {
+                console.error('Error uploading file: ', err);
+                res.status(500).send('Internal server error');
+            } else {
+                console.log('Successfully uploaded file to S3', data.Location);
+                res.send(`File uploaded! <a href="${data.Location}">View file</a>`);
+            }
+        });
     } else {
         res.status(400).send('File upload failed.');
     }
