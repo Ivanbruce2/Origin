af3c25872a0a29175b0e74a896c86bb57a093b56
Ivan <Ivanbruce@hotmail.com>
2023-10-10 09:05:34 -0700

    Signed-off-by: Ivan <Ivanbruce@hotmail.com>

--- server.js
+++ server.js
@@ -1,55 +1,52 @@
-// server.js
-const express = require('express');
+// Import necessary AWS v3 modules
+const { S3Client, PutObjectCommand } = require("@aws-sdk/client-s3");
 const multer = require('multer');
-const AWS = require('aws-sdk');
+const express = require('express');
 const app = express();
 const PORT = 3000;
 
-// Use dotenv to load environment variables
-require('dotenv').config();
-
-// Configure AWS SDK
-AWS.config.update({
-    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-    region: process.env.AWS_REGION  // Updated line
+// Configuring AWS S3 Client
+const s3 = new S3Client({
+    region: process.env.AWS_REGION,
+    credentials: {
+        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    }
 });
 
-const s3 = new AWS.S3();
-
-// Multer storage configuration
-const storage = multer.memoryStorage();
+// Multer configuration for storing file in memory
+const storage = multer.memoryStorage(); 
 const upload = multer({ storage: storage });
 
 app.use(express.static('public'));
 
-// Handle file uploads
-app.post('/upload', upload.single('banner'), (req, res) => {
+// Handling file uploads
+app.post('/upload', upload.single('banner'), async (req, res) => {
     if(req.file) {
         // Parameters for S3 upload
         const params = {
-            Bucket: process.env.AWS_BUCKET_NAME,  // Updated line
+            Bucket: process.env.S3_BUCKET_NAME,
             Key: req.file.originalname,
             Body: req.file.buffer,
             ContentType: req.file.mimetype
         };
 
         // Upload to S3
-        s3.upload(params, function(err, data) {
-            if (err) {
-                console.error('Error uploading file: ', err);
-                res.status(500).send('Internal server error');
-            } else {
-                console.log('Successfully uploaded file to S3', data.Location);
-                res.send(`File uploaded! <a href="${data.Location}">View file</a>`);
-            }
-        });
+        try {
+            const command = new PutObjectCommand(params);
+            const data = await s3.send(command);
+            console.log('Successfully uploaded file to S3');
+            res.send(`File uploaded! <a href="https://${params.Bucket}.s3.${process.env.AWS_REGION}.amazonaws.com/${params.Key}" target="_blank">View file</a>`);
+        } catch (err) {
+            console.error('Error uploading file: ', err);
+            res.status(500).send('Internal server error');
+        }
     } else {
         res.status(400).send('File upload failed.');
     }
 });
 
-// Start server
+// Starting server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
